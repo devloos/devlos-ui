9f5b0256b991ec9a1a849ed1cd50a64bc8731e0d
Carlos Aguilera <18aguilerac@gmail.com>
2025-07-04 23:22:55 -0700

    feat: add OtpInputDemo component and integrate it into App.vue

--- package/playground/src/App.vue
+++ package/playground/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import DragSelectDemo from './demos/DragSelectDemo.vue';
+import OtpInputDemo from './demos/OtpInputDemo.vue';
 </script>
 
 <template>
-  <div>
-    <div>
+  <div class="dui:p-5">
+    <div class="dui:grid-cols-2 dui:grid dui:gap-2 dui:items-center">
       <DragSelectDemo />
+      <OtpInputDemo />
     </div>
   </div>
 </template>
---
+++ package/playground/src/demos/OtpInputDemo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import OtpInput from '@/OtpInput/OtpInput.vue';
+</script>
+
+<template>
+  <div>
+    <OtpInput
+      wrapper-class="dui:flex dui:justify-center dui:gap-2"
+      input-class="dui:border dui:border-cyan-700 dui:rounded dui:focus:outline-cyan-700 dui:focus:outline-offset-4 dui:size-10 dui:text-center"
+      invalid-class="dui:border-red-700 dui:focus:outline-red-700"
+    />
+  </div>
+</template>
